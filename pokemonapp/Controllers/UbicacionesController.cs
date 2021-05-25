8bf4537a46648026a708665da01056284f54f48c
Aylin Santa Cruz Vargas <aylin_santacruz@hotmail.com>
2021-05-25 03:51:27 -0500

    creando vistas en controller

--- pokemonapp/Controllers/UbicacionesController.cs
+++ pokemonapp/Controllers/UbicacionesController.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using pokemonapp.Models;
 
 namespace pokemonapp.Controllers
@@ -12,8 +13,55 @@ namespace pokemonapp.Controllers
         }
 
         public IActionResult Regiones(){
-            var regiones=_context.Regiones.OrderBy(r=> r.Nombre).ToList();
+            //otra opcion para traer los datos de la base de datos a la web
+            var regiones=_context.Regiones.Include(x=>x.Pueblos).OrderBy(r=> r.Nombre).ToList();
+            //var regiones=_context.Regiones.OrderBy(r=> r.Nombre).ToList();
+            return View(regiones);
+        }
+        public IActionResult NuevaRegion(){
             return View();
         }
+        //
+        [HttpPost]
+        public IActionResult NuevaRegion(Region r){
+             if(ModelState.IsValid){
+                _context.Add(r);
+                _context.SaveChanges();
+
+                return RedirectToAction("NuevaRegionConfirmacion");
+            }
+            return View(r);
+        }
+        public IActionResult NuevaRegionConfirmacion(){
+            return View();
+        }
+        [HttpPost]
+        public IActionResult BorrarRegion(int id){
+            var region=_context.Regiones.Find(id);
+            _context.Remove(region);
+            _context.SaveChanges();
+            return RedirectToAction("Regiones");
+            //_context.Regiones.First(r=>r.Id==id);
+        }
+        public IActionResult EditarRegion(int id){
+            var region=_context.Regiones.Find(id);
+            return View(region);
+        }
+        //
+        [HttpPost]
+        public IActionResult EditarRegion(Region r){
+            if(ModelState.IsValid){
+                var region=_context.Regiones.Find(r.Id);
+                region.Nombre=r.Nombre;
+                _context.SaveChanges();
+
+                return RedirectToAction("EditarRegionConfirmacion");
+            }
+            return View(r);
+        }
+        public IActionResult EditarRegionConfirmacion(){
+            return View();
+        }
+
     }
 }
---
+++ pokemonapp/Migrations/20210525070333_MigracionInicial.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace pokemonapp.Migrations
+{
+    public partial class MigracionInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pokemones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nombre = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pokemones", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Regiones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nombre = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Regiones", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tipos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nombre = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tipos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pueblos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nombre = table.Column<string>(type: "text", nullable: true),
+                    RegionId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pueblos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pueblos_Regiones_RegionId",
+                        column: x => x.RegionId,
+                        principalTable: "Regiones",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PokemonTipo",
+                columns: table => new
+                {
+                    PokemonesId = table.Column<int>(type: "integer", nullable: false),
+                    TiposId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PokemonTipo", x => new { x.PokemonesId, x.TiposId });
+                    table.ForeignKey(
+                        name: "FK_PokemonTipo_Pokemones_PokemonesId",
+                        column: x => x.PokemonesId,
+                        principalTable: "Pokemones",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PokemonTipo_Tipos_TiposId",
+                        column: x => x.TiposId,
+                        principalTable: "Tipos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Entrenadores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nombre = table.Column<string>(type: "text", nullable: true),
+                    PuebloId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entrenadores", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Entrenadores_Pueblos_PuebloId",
+                        column: x => x.PuebloId,
+                        principalTable: "Pueblos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Entrenadores_PuebloId",
+                table: "Entrenadores",
+                column: "PuebloId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PokemonTipo_TiposId",
+                table: "PokemonTipo",
+                column: "TiposId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pueblos_RegionId",
+                table: "Pueblos",
+                column: "RegionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Entrenadores");
+
+            migrationBuilder.DropTable(
+                name: "PokemonTipo");
+
+            migrationBuilder.DropTable(
+                name: "Pueblos");
+
+            migrationBuilder.DropTable(
+                name: "Pokemones");
+
+            migrationBuilder.DropTable(
+                name: "Tipos");
+
+            migrationBuilder.DropTable(
+                name: "Regiones");
+        }
+    }
+}
